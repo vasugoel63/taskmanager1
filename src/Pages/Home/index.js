d0721f582efca3f8e7d06b46d91803dda8970c1a
vasu <vasugoel4308@gmail.com>
2024-12-15 20:28:40 +0530

    Your commit message

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router"
 export default function Home(){
     return(
         <>
         <h2>Welcome To Task Manager</h2>
+        <p>
+        <Link to="/login">Login Now</Link>
+      </p>
         </>
     )
 }
